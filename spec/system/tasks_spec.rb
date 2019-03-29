f023cd3db690ac679118542825b939ea504d744d
lime1024 <lime1024@gmail.com>
2019-03-29 14:41:37 +0900

    chapter5 5-16

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,14 +12,16 @@ describe 'タスク管理機能', type: :system do
       fill_in 'パスワード', with: login_user.password
       click_button 'ログインする'
     end
+
+    shared_examples_for 'ユーザ A が作成したタスクが表示される' do
+      it { expect(page).to have_content '最初のタスク' }
+    end
     
     describe '一覧表示機能' do
       context 'ユーザ A がログインしているとき' do
         let(:login_user) { user_a }
 
-        it 'ユーザ A が作成したタスクが表示される' do
-          expect(page).to have_content '最初のタスク'
-        end
+        it_behaves_like 'ユーザ A が作成したタスクが表示される'
       end
 
       context 'ユーザ B がログインしているとき' do
@@ -39,8 +41,34 @@ describe 'タスク管理機能', type: :system do
           visit task_path(task_a)
         end
 
-        it 'ユーザ A が作成したタスクが表示される' do
-          expect(page).to have_content '最初のタスク'
+        it_behaves_like 'ユーザ A が作成したタスクが表示される'
+      end
+    end
+
+    describe '新規作成機能' do
+      let(:login_user) { user_a }
+
+      before do
+        visit new_task_path
+        fill_in '名称', with: task_name
+        click_button '登録する'
+      end
+
+      context '新規作成画面で名称を入力したとき' do
+        let(:task_name) { '新規作成のテストを書く' }
+
+        it '正常に登録される' do
+          expect(page),to have_selector '.alert-success', text: '新規作成のテストを書く'
+        end
+      end
+
+      context '新規作成画面で名称を入力しなかったとき' do
+        let(:task_name) { '' }
+
+        it 'エラーとなる' do
+          within '#error_explanation' do
+            expect(page).to have_content '名称を入力してください'
+          end
         end
       end
     end
